6943b4c7955460bd085832f5bd5a286dbd40cc3b
suji2804 <sujilaxmi.gude@eviden.com>
2025-09-22 18:37:10 +0100

    added MCP server and client for tool calling

--- prod_assistant/retriever/retrieval.py
+++ prod_assistant/retriever/retrieval.py
@@ -1,16 +1,14 @@
 import os
 from langchain_astradb import AstraDBVectorStore
-from typing import List
-from langchain_core.documents import Document
 from utils.config_loader import load_config
 from utils.model_loader import ModelLoader
 from dotenv import load_dotenv
-import sys
-from pathlib import Path
-
+from langchain.retrievers.document_compressors import LLMChainFilter
+from langchain.retrievers import ContextualCompressionRetriever
+from evaluation.ragas_eval import evaluate_context_precision, evaluate_response_relevancy
 # Add the project root to the Python path for direct script execution
-project_root = Path(__file__).resolve().parents[2]
-sys.path.insert(0, str(project_root))
+# project_root = Path(__file__).resolve().parents[2]
+# sys.path.insert(0, str(project_root))
 
 class Retriever:
     def __init__(self):
@@ -20,7 +18,7 @@ class Retriever:
         self.config=load_config()
         self._load_env_variables()
         self.vstore = None
-        self.retriever = None
+        self.retriever_instance = None
     
     def _load_env_variables(self):
         """_summary_
@@ -52,23 +50,76 @@ class Retriever:
                 token=self.db_application_token,
                 namespace=self.db_keyspace,
                 )
-        if not self.retriever:
+        if not self.retriever_instance:
             top_k = self.config["retriever"]["top_k"] if "retriever" in self.config else 3
-            retriever=self.vstore.as_retriever(search_kwargs={"k": top_k})
+            
+            mmr_retriever=self.vstore.as_retriever(
+                search_type="mmr",
+                search_kwargs={"k": top_k,
+                                "fetch_k": 20,
+                                "lambda_mult": 0.7,
+                                "score_threshold": 0.6
+                               })
             print("Retriever loaded successfully.")
-            return retriever
+            
+            llm = self.model_loader.load_llm()
+            
+            compressor=LLMChainFilter.from_llm(llm)
+            
+            self.retriever_instance = ContextualCompressionRetriever(
+                base_compressor=compressor, 
+                base_retriever=mmr_retriever
+            )
+            
+        return self.retriever_instance
             
     def call_retriever(self,query):
         """_summary_
         """
         retriever=self.load_retriever()
         output=retriever.invoke(query)
+        print(f"Retrieved {len(output)} documents.")
         return output
     
 if __name__=='__main__':
-    retriever_obj = Retriever()
-    user_query = "what is my name?"
-    results = retriever_obj.call_retriever(user_query)
+    user_query = "What is the price of iPhone 15?"
     
-    for idx, doc in enumerate(results, 1):
-        print(f"Result {idx}: {doc.page_content}\nMetadata: {doc.metadata}\n")
+    retriever_obj = Retriever()
+    
+    retrieved_docs = retriever_obj.call_retriever(user_query)
+    
+    def _format_docs(docs) -> str:
+        if not docs:
+            return "No relevant documents found."
+        formatted_chunks = []
+        for d in docs:
+            meta = d.metadata or {}
+            formatted = (
+                f"Title: {meta.get('product_title', 'N/A')}\n"
+                f"Price: {meta.get('price', 'N/A')}\n"
+                f"Rating: {meta.get('rating', 'N/A')}\n"
+                f"Reviews:\n{d.page_content.strip()}"
+            )
+            formatted_chunks.append(formatted)
+        return "\n\n---\n\n".join(formatted_chunks)
+    
+    retrieved_contexts = [_format_docs(retrieved_docs) ]
+    for doc in retrieved_docs:
+        print(f"retrieved----{doc}")
+    
+    #this is not an actual output this have been written to test the pipeline
+    # response="iphone 16 plus, iphone 16, iphone 15 are best phones under 1,00,000 INR."
+    
+    # context_score = evaluate_context_precision(user_query,response,retrieved_contexts)
+    # relevancy_score = evaluate_response_relevancy(user_query,response,retrieved_contexts)
+    
+    # print("\n--- Evaluation Metrics ---")
+    # print("Context Precision Score:", context_score)
+    # print("Response Relevancy Score:", relevancy_score)
+    
+
+    
+    
+    
+    # for idx, doc in enumerate(results, 1):
+    #     print(f"Result {idx}: {doc.page_content}\nMetadata: {doc.metadata}\n")
